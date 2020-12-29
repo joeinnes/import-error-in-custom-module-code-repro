a455ee1f4519577dbb29be45831b4bf0664e6a08
Joe Innes <joe@joeinn.es>
2020-12-29 11:23:36 +0100

    Comment update

--- modules/pesky-page/ui/public/lib/db.js
+++ modules/pesky-page/ui/public/lib/db.js
@@ -1,4 +1,4 @@
-// Just export the function, don't do anything fancy.
+// Just export the function, don't do anything fancy... although the import will never be attempted, I'll keep this here to rule out anything weird happening with an import failing.
 
 export default function db(input) {
   console.log(input);
